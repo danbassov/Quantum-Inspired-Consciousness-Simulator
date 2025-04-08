54955de56df5e90017c87046e860d526069401ab
danbassov <126057149+danbassov@users.noreply.github.com>
2025-04-08 10:16:23 -0400

    Delete web directory

--- web/main.js
+++
@@ -1,9 +0,0 @@
-import init, { QuantumNeuralNetwork } from './pkg/quantum_consciousness.js';
-
-async function run() {
-    await init();
-    const network = QuantumNeuralNetwork.new(10);
-    console.log(network.decide());
-}
-
-run();
